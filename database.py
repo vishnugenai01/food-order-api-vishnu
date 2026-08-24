9b38d7e9d5564bc64c6a36e044dde986a056a0c0
Vishnu Vardhan <vishnugenai01@gmail.com>
2026-08-24 15:45:58 +0530

    Fix Render deployment

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:root@localhost:5432/fastapitask"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
 engine = create_engine(DATABASE_URL)
 
